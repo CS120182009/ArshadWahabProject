b65f347daf7c4c8145c57133472361990a136cf4
Muhammad Ishtiaq <71250626+MUHAMMAD343@users.noreply.github.com>
2020-09-19 21:04:23 +0500

    First Project

---
+++ Sports system/Sports system/Controllers/PlayersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sports_system.Data;
+using Sports_system.Models;
+
+namespace Sports_system.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private Sports_systemContext db = new Sports_systemContext();
+
+        // GET: Players
+        public ActionResult Index(string searchString)
+        {
+            var players = from m in db.Players
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                players = players.Where(s => s.Reg_NO.Contains(searchString));
+            }
+
+            return View(players);
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Father_Name,Reg_NO,age,department,Program,Contact,Address")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Father_Name,Reg_NO,age,department,Program,Contact,Address")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Players.Find(id);
+            db.Players.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sports system/Sports system/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sports_system.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Father_Name { get; set; }
+        public string Reg_NO { get; set; }
+        public string age { get; set; }
+        public string department { get; set; }
+        public string Program { get; set; }
+        public int Contact { get; set; }
+        public string Address { get; set; }
+    }
+}
